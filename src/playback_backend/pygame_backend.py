0311f93b11dca74f5ccd9cd5446f0bb68cb1f529
Nmstr <publicNamester@gmail.com>
2024-08-17 22:12:18 +0200

    first example with pygame backend

---
+++ src/playback_backend/pygame_backend.py
@@ -0,0 +1,56 @@
+import pygame
+
+class PygameBackend:
+    def __init__(self):
+        pygame.mixer.init()
+
+    def load(self, file_path: str):
+        """
+        Load the song.
+        """
+        pygame.mixer.music.load(file_path)
+
+    def play(self, start_time: float = 0):
+        """
+        Play the song.
+        """
+        pygame.mixer.music.play(start=start_time)
+
+    def pause(self):
+        """
+        Pause the song.
+        """
+        pygame.mixer.music.pause()
+
+    def unpause(self):
+        """
+        Unpause the song.
+        """
+        pygame.mixer.music.unpause()
+
+    def stop(self):
+        """
+        Stop the song.
+        """
+        pygame.mixer.music.stop()    
+
+    def set_volume(self, volume: float):
+        """
+        Set the volume between 0 and 1.
+
+        Parameters:
+            volume (float): Volume between 0 and 1.
+        """
+        pygame.mixer.music.set_volume(volume)
+
+    def get_volume(self) -> float:
+        """
+        Get the volume.
+        """
+        return pygame.mixer.music.get_volume()
+
+    def get_busy(self) -> bool:
+        """
+        Check if the song is playing.
+        """
+        return pygame.mixer.music.get_busy()
