b3be69604ab716ac770482176157930b0f11cc8b
Nmstr <publicNamester@gmail.com>
2024-08-18 13:32:17 +0200

    added type annotations

--- test/backend_pygame_test.py
+++ test/backend_pygame_test.py
@@ -7,32 +7,32 @@ def audio_file(request):
     base_file_name = "test/audio_files/75hz_stereo"
     yield base_file_name + "." + format
 
-def test_pygame_audio_player_play(audio_file):
+def test_pygame_audio_player_play(audio_file: str) -> None:
     player = AudioPlayer(audio_file, 'pygame')
     player.play()
     assert player._playback_backend.get_busy() == True
     assert player._is_paused == False
 
-def test_pygame_audio_player_pause(audio_file):
+def test_pygame_audio_player_pause(audio_file: str) -> None:
     player = AudioPlayer(audio_file, 'pygame')
     player.play()
     player.pause()
     assert player._playback_backend.get_busy() == False
     assert player._is_paused == True
 
-def test_pygame_audio_player_stop(audio_file):
+def test_pygame_audio_player_stop(audio_file: str) -> None:
     player = AudioPlayer(audio_file, 'pygame')
     player.play()
     player.stop()
     assert player._playback_backend.get_busy() == False
     assert player._is_paused == False
 
-def test_pygame_audio_player_position(audio_file):
+def test_pygame_audio_player_position(audio_file: str) -> None:
     player = AudioPlayer(audio_file, 'pygame')
     player.play()
     assert player.position > 0
 
-def test_pygame_audio_player_volume(audio_file):
+def test_pygame_audio_player_volume(audio_file: str) -> None:
     player = AudioPlayer(audio_file, 'pygame')
     assert player.volume == 1.0
     player.volume = 0.5
