001b5ac2e5b67d68a4303b62e4e6a88e8b7546eb
Nmstr <publicNamester@gmail.com>
2024-08-19 21:01:19 +0200

    improved backend load errors

--- PySoundSphere/playback_backend/backend_loader.py
+++ PySoundSphere/playback_backend/backend_loader.py
@@ -16,21 +16,21 @@ def load_backend(backend_name: str) -> object:
             from .sounddevice_backend import SounddeviceBackend
             return SounddeviceBackend()
         except ImportError as e:
-            raise ValueError(f'Failed to load backend "{backend_name}" with error: "{e}"')
+            raise ValueError(f'Failed to load backend "{backend_name}" with error: "{e}". Is it installed?')
 
     elif backend_name == "pygame":
         try:
             from .pygame_backend import PygameBackend
             return PygameBackend()
         except ImportError as e:
-            raise ValueError(f'Failed to load backend "{backend_name}" with error: "{e}"')
+            raise ValueError(f'Failed to load backend "{backend_name}" with error: "{e}". Is it installed?')
         
     elif backend_name == "ffplay":
         try:
             from .ffplay_backend import FfplayBackend
             return FfplayBackend()
         except ImportError as e:
-            raise ValueError(f'Failed to load backend "{backend_name}" with error: "{e}"')
+            raise ValueError(f'Failed to load backend "{backend_name}" with error: "{e}". Is it installed?')
 
     else:
         raise ValueError(f'Backend "{backend_name}" not found')
